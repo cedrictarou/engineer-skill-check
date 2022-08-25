8e0b79a1a94cdd28a9c33fac5e7dc85b854a0678
cedrictaro-g <cedric.tarou@gmail.com>
2022-08-25 18:54:18 +0900

    add code to sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,9 +2,9 @@ module SessionsHelper
   def login(user)
     session[:user_id] = user.id
   end
-  
+
   def current_user
-    @current_user ||= Employee.find_by(id: session[:user_id])
+    @current_user ||= Employee.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def logged_in?
